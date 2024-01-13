7bbb6fbb144f0613d34a8f1240ce3e177729e48c
Toss <126696033+B-Henslee@users.noreply.github.com>
2024-01-13 20:26:41 +0100

    Remove prompt and add button selection for playerChoice

--- rps.js
+++ rps.js
@@ -1,44 +1,47 @@
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+        // Call the getPlayerChoice function with the button's ID when it is clicked
+        const playerChoice = getPlayerChoice(button.id);
+
+        // Call the playRound function with the player's choice
+        playRound(playerChoice);
+  });
+});
+
+function getPlayerChoice (playerChoice){
+    return playerChoice;
+}
+
 function getComputerChoice (){
     const choices = ["rock","paper","scissors"]
-    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return (computerChoice);
 }
 
-function getPlayerChoice (){
-    let playerChoice = prompt("rock paper scissors?")
-    return (playerChoice);
-}
 
-function playRound(playerChoice,computerChoice){
+function playRound(playerChoice){
+    let computerChoice=getComputerChoice();
         if (playerChoice === "rock" && computerChoice === "scissors" || 
             playerChoice === "paper" && computerChoice === "rock" ||
             playerChoice === "scissors" && computerChoice === "paper"){
+                console.log("You chose: ", playerChoice, "\nComputer choice: ", computerChoice);
                 console.log("you win");
         }
         else if (playerChoice === "rock" && computerChoice === "paper" || 
             playerChoice === "paper" && computerChoice === "scissors" ||
             playerChoice === "scissors" && computerChoice === "rock"){
+                console.log("You chose: ", playerChoice, "\nComputer choice: ", computerChoice);
                 console.log ("you lose");
                 }
         else {
+            console.log("You chose: ", playerChoice, "\nComputer choice: ", computerChoice);
             console.log("draw")
         }
-    return(playerChoice,computerChoice)
+
 }
 
-function playGame(){
-        let playerChoice=getPlayerChoice();
-        let computerChoice=getComputerChoice();
-        console.log("You chose: ", playerChoice, "\nComputer choice: ", computerChoice);
-        game=playRound(playerChoice,computerChoice);
-}
-
-playGame();
-
-/* 
-Get user choice from button click
-run getComputerchoice
-get winner
-update score
-alert box is player wins/loses 5
-*/
+playRound();
